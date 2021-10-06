29b43afe7e31dd1fb36cf6abfe6da0c19815d644
KronosGR <geo.elgeo@gmail.com>
2021-10-06 16:05:58 +0200

    AddUser component added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AddUser from './components/Users/AddUser';
+
+
+function App() {
+  return (
+    <div>
+      <AddUser></AddUser>
+
+    </div>
+  );
+}
+
+export default App;
